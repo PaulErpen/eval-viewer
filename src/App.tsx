00c83695654192761834f2b6384eb46ee36566b3
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-12-26 12:47:53 +0100

    Working example

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import "./App.scss";
 export const App = () => {
   return (
     <div className="app-container">
-      <GSViewer plyPath="/home/paul/TUWien/MasterThesisNerf/repos/GaussianSplats3D/tests/example_splats/ignatius-mcmc-2-downscale-2-uncapped_model.ply" />
+      <GSViewer plyPath="lego_default_big.ply" />
     </div>
   );
 };
-
-// <GSViewer plyPath="/home/paul/TUWien/MasterThesisNerf/repos/GaussianSplats3D/tests/example_splats/ignatius-3968-uncapped_model.ply" />
