bffd686c87d720013796b36ffb45687a0e5d18a1
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-01-09 10:40:21 +0100

    Implement model switching

--- src/components/eval-handler/eval-handler.tsx
+++ src/components/eval-handler/eval-handler.tsx
@@ -6,8 +6,7 @@ import "./eval-handler.scss";
 export const EvalHandler = () => {
   const { evaluationService } = useServiceContext();
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [hideFirstModel, setHideFirstModel] = useState<boolean>(false);
-  const [hideSecondModel, setHideSecondModel] = useState<boolean>(true);
+  const [showFirstModel, setShowFirstModel] = useState<boolean>(false);
 
   evaluationService.connectLoadingState(setIsLoading);
 
@@ -21,24 +20,11 @@ export const EvalHandler = () => {
 
   return (
     <div className="eval-handler">
-      {!isLoading && firstPlyUrl !== null && (
+      {!isLoading && firstPlyUrl !== null && secondPlyUrl !== null && (
         <GSViewer
-          plyPath={firstPlyUrl}
-          hideModel={hideFirstModel}
-          rotation_w={currentPair ? currentPair.rotation[0] : 0}
-          rotation_qx={currentPair ? currentPair.rotation[1] : 0}
-          rotation_qy={currentPair ? currentPair.rotation[2] : 0}
-          rotation_qz={currentPair ? currentPair.rotation[3] : 0}
-          position_x={currentPair ? currentPair.position[0] : 0}
-          position_y={currentPair ? currentPair.position[1] : 0}
-          position_z={currentPair ? currentPair.position[2] : 0}
-        />
-      )}
-
-      {!isLoading && secondPlyUrl !== null && (
-        <GSViewer
-          plyPath={secondPlyUrl}
-          hideModel={hideSecondModel}
+          plyPath1={firstPlyUrl}
+          plyPath2={secondPlyUrl}
+          showFirst={showFirstModel}
           rotation_w={currentPair ? currentPair.rotation[0] : 0}
           rotation_qx={currentPair ? currentPair.rotation[1] : 0}
           rotation_qy={currentPair ? currentPair.rotation[2] : 0}
@@ -53,8 +39,7 @@ export const EvalHandler = () => {
         <button
           className="switch"
           onClick={() => {
-            setHideFirstModel((prev) => !prev);
-            setHideSecondModel((prev) => !prev);
+            setShowFirstModel((prev) => !prev);
           }}
         >
           Switch model
--- src/components/gs-viewer/gs-viewer.tsx
+++ src/components/gs-viewer/gs-viewer.tsx
@@ -6,8 +6,9 @@ import "./gs-viewer.scss";
 // @ts-ignore
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 export interface GSViewerProps {
-  plyPath: String;
-  hideModel: boolean;
+  plyPath1: String;
+  plyPath2: String;
+  showFirst: boolean;
   rotation_w: number;
   rotation_qx: number;
   rotation_qy: number;
@@ -29,8 +30,9 @@ const resizeRendererToDisplaySize = (renderer: THREE.Renderer) => {
 };
 
 export const GSViewer = ({
-  plyPath,
-  hideModel,
+  plyPath1,
+  plyPath2,
+  showFirst,
   rotation_w,
   rotation_qx,
   rotation_qy,
@@ -42,7 +44,7 @@ export const GSViewer = ({
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    if (canvas && plyPath && plyPath.length > 0) {
+    if (canvas && plyPath1 && plyPath1.length > 0) {
       const scene = new THREE.Scene();
       const renderer = new THREE.WebGLRenderer({
         antialias: false,
@@ -55,23 +57,39 @@ export const GSViewer = ({
       controls.enableDamping = true;
       camera.position.z = 2;
 
-      const gsViewer = new GaussianSplats3D.DropInViewer({
-        integerBasedSort: false,
-        sphericalHarmonicsDegree: 0,
-      });
-      gsViewer.addSplatScene(plyPath, {
-        progressiveLoad: true,
-        format: 1,
-        rotation: [rotation_w, rotation_qx, rotation_qy, rotation_qz],
-        position: [position_x, position_y, position_z],
-      });
-      scene.add(gsViewer);
+      const gsViewer1 = createViewer(
+        plyPath1,
+        rotation_w,
+        rotation_qx,
+        rotation_qy,
+        rotation_qz,
+        position_x,
+        position_y,
+        position_z
+      );
+      scene.add(gsViewer1);
+
+      const gsViewer2 = createViewer(
+        plyPath2,
+        rotation_w,
+        rotation_qx,
+        rotation_qy,
+        rotation_qz,
+        position_x,
+        position_y,
+        position_z
+      );
+      scene.add(gsViewer2);
 
       const render = () => {
         requestAnimationFrame(render);
 
-        if (renderer.domElement.style.display == "none") {
-          return;
+        if (renderer.domElement.dataset.showFirst === "true") {
+          gsViewer1.visible = true;
+          gsViewer2.visible = false;
+        } else {
+          gsViewer1.visible = false;
+          gsViewer2.visible = true;
         }
 
         if (resizeRendererToDisplaySize(renderer)) {
@@ -86,14 +104,33 @@ export const GSViewer = ({
       };
       requestAnimationFrame(render);
     }
-  }, [plyPath, canvas]);
+  }, [plyPath1, plyPath2, canvas]);
 
   return (
-    <div className="viewer" data-model-path={plyPath}>
-      <canvas
-        ref={setCanvas}
-        style={{ display: hideModel ? "none" : "block" }}
-      />
+    <div className="viewer" data-model-path={plyPath1}>
+      <canvas ref={setCanvas} data-show-first={showFirst} />
     </div>
   );
 };
+function createViewer(
+  plyPath1: String,
+  rotation_w: number,
+  rotation_qx: number,
+  rotation_qy: number,
+  rotation_qz: number,
+  position_x: number,
+  position_y: number,
+  position_z: number
+) {
+  const gsViewer1 = new GaussianSplats3D.DropInViewer({
+    integerBasedSort: false,
+    sphericalHarmonicsDegree: 0,
+  });
+  gsViewer1.addSplatScene(plyPath1, {
+    progressiveLoad: true,
+    format: 1,
+    rotation: [rotation_w, rotation_qx, rotation_qy, rotation_qz],
+    position: [position_x, position_y, position_z],
+  });
+  return gsViewer1;
+}
