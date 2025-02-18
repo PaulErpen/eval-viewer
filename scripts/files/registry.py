6e03e04af2eeaa350f07e437fdb1050ceae58d10
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-02-18 10:30:09 +0100

    Update registry

--- scripts/files/registry.py
+++ scripts/files/registry.py
@@ -25,42 +25,42 @@ def get_file_registry(data_dir: str) -> List[ModelFile]:
                 if technique in ["default", "mcmc"]:
                     model_files.append(
                         ModelFile(
-                            f"{data_dir}/models/{technique}/{technique}-{dataset}-{size}-1/{technique}-{dataset}-{size}-1_model.ply",
+                            f"{data_dir}/models/{technique}/{technique}-{dataset}-{size}-3/{technique}-{dataset}-{size}-3_model.ply",
                             f"{technique}-{dataset}-{size}-1",
                         )
                     )
                 elif technique == "mini_splatting":
                     model_files.append(
                         ModelFile(
-                            f"{data_dir}/models/mini-splatting/mini_splatting-{dataset}-{size}-1/point_cloud/iteration_30000/point_cloud.ply",
+                            f"{data_dir}/models/mini-splatting/mini_splatting-{dataset}-{size}-3/point_cloud/iteration_30000/point_cloud.ply",
                             f"mini_splatting-{dataset}-{size}-1",
                         ),
                     )
                 elif technique == "eagles":
                     model_files.append(
                         ModelFile(
-                            f"{data_dir}/models/eagles/eagles-{dataset}-{size}-1/point_cloud/iteration_30000/point_cloud.ply",
+                            f"{data_dir}/models/eagles/eagles-{dataset}-{size}-3/point_cloud/iteration_30000/point_cloud.ply",
                             f"eagles-{dataset}-{size}-1",
                         ),
                     )
                 elif technique == "mip_splatting":
                     model_files.append(
                         ModelFile(
-                            f"{data_dir}/models/mip-splatting/mip_splatting-{dataset}-{size}-1/point_cloud/iteration_30000/point_cloud.ply",
+                            f"{data_dir}/models/mip-splatting/mip_splatting-{dataset}-{size}-3/point_cloud/iteration_30000/point_cloud.ply",
                             f"mip_splatting-{dataset}-{size}-1",
                         ),
                     )
                 elif technique == "gaussian_pro":
                     model_files.append(
                         ModelFile(
-                            f"{data_dir}/models/gaussian-pro/gaussian_pro-{dataset}-{size}-1/point_cloud/iteration_30000/point_cloud.ply",
+                            f"{data_dir}/models/gaussian-pro/gaussian_pro-{dataset}-{size}-3/point_cloud/iteration_30000/point_cloud.ply",
                             f"gaussian_pro-{dataset}-{size}-1",
                         ),
                     )
                 elif technique == "geo_gaussian":
                     model_files.append(
                         ModelFile(
-                            f"{data_dir}/models/geogaussian/geo_gaussian-{dataset}-{size}-1/point_cloud/iteration_30000/point_cloud.ply",
+                            f"{data_dir}/models/geogaussian/geo_gaussian-{dataset}-{size}-3/point_cloud/iteration_30000/point_cloud.ply",
                             f"geo_gaussian-{dataset}-{size}-1",
                         ),
                     )
